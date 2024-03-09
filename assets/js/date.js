83290f240dde8f090d1aa7692d362d50ece095a3
Tolux001 <toluwalasepeter100@gmail.com>
2024-03-10 00:43:35 +0100

    COmmit files

---
+++ assets/js/date.js
@@ -0,0 +1,13 @@
+const email = "lennoxhub[at]hotmail[dot]com";
+const emailLink = document.createElement("a");
+const pos = document.querySelector(".emailProtection");
+
+emailLink.href = "mailto:" + email.replace("[at]", "@").replace("[dot]", ".");
+emailLink.textContent = "[Email Protected]";
+
+pos.appendChild(emailLink);
+
+let dateTime = document.querySelector(".dateTime");
+let currentDate = new Date().getFullYear();
+
+dateTime.textContent = currentDate;
